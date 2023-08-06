4e05ce446231690607181a913aef2aafd984cdf4
caleb127 <matovucaleb@lbtvug.com>
2023-08-06 12:31:54 +0000

    pushed

--- archive/MATH/main.py
+++ archive/MATH/main.py
@@ -1,3 +1,14 @@
 import torch
 import tensorflow
+import nltk
 from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.tokenize import PunktSentenceTokenizer
+
+Tokenizer_wrd = TreebankWordTokenizer()
+print(Tokenizer_wrd.tokenize(
+  'How many vertical asymptotes does the graph of $y=\\frac{2}{x^2+x-6}$ have?'
+))
+word_stemmer = PorterStemmer()
+print(word_stemmer.stem('writing'))
